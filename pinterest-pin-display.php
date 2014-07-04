fc13a5cd4bc8d57295bf1827337f1e96faba5e08
screenwave <screenwave@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-04 18:41:37 +0000

    stable release version
    git-svn-id: https://plugins.svn.wordpress.org/pinterest-pin-display/trunk@943511 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pinterest-pin-display.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * @package pinterest-pin-display
+*/
+/*
+Plugin Name: Pinterest Pin Display
+Plugin URI: http://www.screenwave.net
+Description: Pinterest Pin Display is a Shortcode Plugin for Wordpress. Pinterest Pin Display helps you to display Pinterest Profile or Board on your wordpress website.
+Version: 0.0.1
+Author: Michael Wilkinson
+Author URI: http://www.screenwave.net
+*/
+
+add_shortcode('pinterest-pin', 'pinterest_pin_display');
+
+function pinterest_pin_display($atts){
+    $atts = shortcode_atts(array(
+ 		'display' => 'Profile',
+ 		'pin_account' => 'britneyspears',
+                'display_title' => 'yes',
+ 		'display_description' => 'yes',
+                'img_show' => '20',
+ 		'img_width' => '60',
+                'img_height' => '60',
+ 		'img_distance' => '3',
+                'box_width' => '280',
+ 		'box_height' => '350',
+                'border_color' => '#000000',
+                'border_size' => '3',
+ 		'border_radius' => '5',
+                'bg_color' => '#DEDEDE',
+ 		'follow' => 'yes'
+ 	), $atts);
+    extract($atts);
+         /*
+         * Starting Action
+         */
+        
+        // Profile or Board
+        if($display == 'Board'){
+	$xml_url = "http://pinterest.com/$pin_account/rss";
+	}
+        else{
+	$xml_url = "http://pinterest.com/$pin_account/feed.rss";
+	}
+        
+        $xml = simplexml_load_file($xml_url);
+        
+	$image_show  = $img_show+1;
+        
+        ?>
+<!-- Internal Stylesheets of the Plugin -->  
+<style>
+  .pin{ width:<?php echo $box_width;?>px; height:<?php echo $box_height;?>px; border:<?php echo $border_size;?>px solid <?php echo $border_color;?>; margin:0 auto; border-radius:<?php echo $border_radius;?>px; background:<?php echo $bg_color;?>; padding:5px}
+  .pin img{ height:<?php echo $img_height;?>px; width:<?php echo $img_width;?>px; float:left; border:1px solid #333; margin:<?php echo $img_distance;?>px; padding:1px }
+  .pin p {margin: 0;}
+  .pin img:hover{ -ms-transform: scale(3,3); /* IE 9 */
+    -webkit-transform: scale(3,3); /* Chrome, Safari, Opera */
+    transform: scale(3,3); z-index:1000 !important}
+	.follow {padding-bottom: 10px;}	 
+	.title h4 { margin: 0; text-align: center;}
+   .title p { margin: 0; padding-bottom: 10px;}
+</style>
+<div class="pinterest_pin_display_widget">
+<?php
+    $pin = $xml->channel;
+    $tiles = "";
+    $descriptions = "";
+    ?>
+    <div class="title">
+        <h1> <?php if ($display_title=='yes') {?><a href=<?php echo "$pin->link";?> target="_blank"><?php echo $pin->title;?></a>
+    <?php } else { echo $tiles; } ?></h1>
+             <?php if ($display_description=='yes') {?> <p> <?php  echo $pin->description;?></p><?php } else { echo $descriptions; }?>
+         </div>
+	<!-- follow button start here -->
+	<div class="follow">	
+         <?php  $follows='';?>
+         
+         <?php if($follow == 'yes'){?> 
+            <a data-pin-do="buttonFollow" href="http://www.pinterest.com/<?php echo $account;?>/">Pinterest</a>
+            <!-- Please call pinit.js only once per page -->
+            <script type="text/javascript" async src="//assets.pinterest.com/js/pinit.js"></script>
+         <?php } else{ echo $follows; } ?>
+    </div>
+	<!-- follow button end here -->
+   <div class="pin">
+	
+       <?php 
+	$l = $image_show;
+        $c = 0 ;
+        foreach($xml->channel->item as $pinterest){
+                   $c++;
+	if($c == $l)
+                    break;
+                 $pin_desc = $pinterest->description ;
+                 $link     = $pinterest->link ;
+                 $doc = new DOMDocument();
+                 $doc->loadHTML($pin_desc);
+                 $imageTags = $doc->getElementsByTagName('img');
+                 $pin_print = "";
+             
+        foreach($imageTags as $tag)    {
+               echo "<a href='" . $link . "' target='_blank'>" . "<img src='" . $tag->getAttribute('src') . "'/></a>";
+             }
+          } /*<!--end first foreach bracket-->*/
+?>
+   </div>
+</div>
+<?php
+    return;
+}
---
+++ readme.txt
@@ -0,0 +1,129 @@
+=== Pinterest Pin Display ===
+Contributors: screenwave
+Tags: pinterest, pinterest display shortcode, pinterest display, wordpress plugin, pinterest shortcode
+Requires at least: 3.0.1
+Tested up to: 3.9.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Pinterest Pin Display is a Shortcode Plugin for Wordpress. Pinterest Pin Display helps you to display Pinterest Profile or Board on your wordpress website.
+
+== Description ==
+
+Pinterest is a very popular website.
+
+A few specific feature which made our widget unique than other available wordpress pinterest widgets :
+
+*   Fully Customizable. You can take control over everything as this module is not just display stuffs from Pinterest Website.
+*   Pinterest API for XML Feed Implemented.
+*   Show Latest Pins from Pinterest Profile or Board Display.
+*   Tested on all latest wordpress versions. Also we've tested it on older versions to make sure everything work smoothly for you.
+*   Stable Version Released.
+
+== Installation ==
+
+This section describes how to install the plugin (Widget) and get it working.
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+How to Display the Plugin Shortcode in Wordpress Website:
+
+Shortcode:
+
+[pinterest-pin]
+
+You can put that on your wordpress post or page. It will display intially a Pinterest Profile with all default values setup on the plugin.
+
+To Put your own Pinterest Profile you have to setup few attributes. They are describing below --
+
+Pinterest Profile Display Shortcode:
+
+[pinterest-pin display='Profile' pin_account='YOUR_PINTEREST_PROFILE_WILL_BE_HERE']
+
+If your Pinterest Profile URL is : http://www.pinterest.com/britneyspears
+
+Then This Field value will be : britneyspears
+
+example: [pinterest-pin display='Profile' pin_account='britneyspears']
+
+Pinterest Board Display Shortcode:
+
+[pinterest-pin display='Board' pin_account='YOUR_PINTEREST_BOARD_WILL_BE_HERE']
+
+For Board: If you Board URL is : http://www.pinterest.com/pinterest/pin-pets
+
+Then this field value will be : pinterest/pin-pets
+
+example: [pinterest-pin display='Board' pin_account='pinterest/pin-pets']
+
+Their are other attributes available to make your life easy - They are described below:
+
+
+
+* Display Title: YES to Display - NO to Hide
+
+display_title='yes' OR title='no'
+
+* Display Description: YES to Display - NO to Hide
+
+display_description='yes' OR title='no'
+
+* Number of Image: No. of image you like to display in your module.
+
+img_show='NUMERIC_NUMBER_LIMIT' - example: img_show='20'
+
+* Image Width: Width of your pin image in px.
+
+img_width='60'
+
+* Image Height: Height of your pin image in px.
+
+img_heigh='60'
+
+* Image Spacing: Spacing between images in px.
+
+img_distance='3'
+
+* Box Width: Box Width of your image in px.
+
+box_width='280'
+
+* Box Height: Box Height of your image in px.
+
+box_height='350'
+
+* Border Color: Border Color - Can be pick from the color picker.
+
+border_color='#000000'
+
+* Border Width: Border Size in px.
+
+border_size='3'
+
+Border Radius: Border Radius Size.
+
+border_radius='5'
+
+* Box Background Color: Box Background color can pick using the color picker from Joomla Module manager Backend.
+
+bg_color='#DEDEDE'
+
+Show Follow Button: YES to Display - NO to Hide.
+
+follow='yes' OR follow='no'
+
+== Frequently Asked Questions ==
+
+= Can I display both Pinterest Profile and Pinterest Board in same page using this widget? =
+
+Yes you can. Make sure the CSS overrides.
+
+== Screenshots ==
+
+1. Display of our Plugin
+
+== Changelog ==
+
+= 1.0 =
+* Stable version released
